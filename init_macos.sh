9a808ec2abe15340bb492f07cd1b3260186c429d
Yoshi MAKI <ymaki620@gmail.com>
2017-04-13 01:47:07 +0900

    modify source

--- init_macos.sh
+++ init_macos.sh
@@ -3,8 +3,15 @@
 # 
 #git clone git@github.com:ymaki620/dotfiles.git ~/.dotfiles_github
 
+
+# set bashrc
 if [ -f ~/.bashrc ]; then
     mv ~/.bashrc ~/.bashrc_org
 fi
-
 ln -s /Users/$USER/.dotfiles_github/bashrc_macos /Users/$USER/.bashrc
+
+# set bash_profile
+if [ -f ~/.bash_profile ]; then
+    mv ~/.bash_profile ~/.bash_profile_org
+fi
+ln -s /Users/$USER/.dotfiles_github/bash_profile_macos /Users/$USER/.bash_profile
